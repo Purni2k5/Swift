362a9c51a65d5451172f5f3958c1f29dac841efd
siddhantp <martand.joshi@loylty.com>
2017-12-04 19:48:38 +0530

    first commit for swift

---
+++ SwiftSimpleDemo/SwiftSimpleDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSimpleDemo
+//
+//  Created by Siddhant on 04/12/17.
+//  Copyright © 2017 Loylty Rewardz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
